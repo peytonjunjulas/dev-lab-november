e9240a1462ea89b2fb72e7b2966beae7196c8362
peyton_junjulas <prjunjulas@gmail.com>
2022-08-30 13:18:00 -0500

    first commit setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const path = require("node:path")
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+ 
+app.listen(3000)
