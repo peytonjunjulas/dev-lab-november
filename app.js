50a332d00b10d7894340e77ec58bddb86bef3be4
peyton_junjulas <prjunjulas@gmail.com>
2022-09-01 13:18:59 -0500

    added nodemon and send file to index

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express()
 const path = require("node:path")
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  //res.send('Hello Node/Express/Heroku')
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
  
 app.listen(process.env.PORT || 3000,
