48fde601faf778bd9a3cbd33f43f1f2fbbc69d32
peyton_junjulas <prjunjulas@gmail.com>
2022-09-06 13:08:20 -0500

    submit button

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
+// const req = require('express/lib/request')
+// const res = require('express/lib/response')
 const path = require("node:path")
-
+const bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs");
 
  
-let userNameExpress = "Joe";
+let userName = "Peyton Page for express";
+let thatData = "";
 
 app.get('/', function (req, res) {
-  res.render('index', { userNameEjs: userNameExpress});
+  res.render('index', { userName: userName});
+})
+
+app.post("/saveToNode", (req, res) => {
+  console.log(req.body);
+  console.group(req.body.userName);
+  res.render('index', { userName: req.body.userName });
 })
  
 app.listen(process.env.PORT || 3000,
